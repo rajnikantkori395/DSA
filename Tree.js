83aa083c233ef088e7652bf05915cee4d0ee5362
Gulshan196 <109276430+Gulshan196@users.noreply.github.com>
2022-07-26 15:14:22 +0530

    tree implimentation

---
+++ Tree.js
@@ -0,0 +1,24 @@
+class TreeNode {
+    constructor(value) {
+        this.value = value;  
+        this.left = null; 
+        this.right = null;
+    }
+}
+
+class BinaryTree {
+    constructor(){
+        this .root=null;
+    }
+    insert(data,key){
+        //  it creates a new node to be inserted and calls insertNode()
+        var newNode=new TreeNode(data);
+        if(this.root===null)
+        this.root=newNode;
+        else
+        if(key.left==null){
+
+        }
+       
+    }
+}
